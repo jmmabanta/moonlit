423e45b12ed15c4aaa37c03387a0a841fa2bcd84
John M <johnmabanta51@gmail.com>
2021-10-06 16:53:20 -0600

    Fixed domain name for CORS

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/": {"origins":
                             ["http://localhost:3000",
-                                "https://jmmabanta.github.io/moonlit"]
+                                "https://jmmabanta.github.io"]
                             }})
 
 
