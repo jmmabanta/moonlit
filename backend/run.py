6969cce95d87e8aae414862dbb7f39c2f2e2c90c
John M <johnmabanta51@gmail.com>
2021-11-07 12:37:38 -0700

    Fixed CORS from refactor

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,10 @@
+from flask_cors import CORS
 from moonlit_api import create_app
 
 if __name__ == '__main__':
     app = create_app()
+    CORS(app, resources={r"/*": {"origins":
+                                 ["http://localhost:3000",
+                                  "https://jmmabanta.github.io"]
+                                 }})
     app.run(debug=True, host='0.0.0.0')
