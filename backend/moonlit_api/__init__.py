4f6f7a285cb5e5eb817dbd43dd7bfd3e2b7609ce
John M <johnmabanta51@gmail.com>
2021-11-07 12:41:55 -0700

    Now is CORS fixed?

--- backend/moonlit_api/__init__.py
+++ backend/moonlit_api/__init__.py
@@ -8,6 +8,10 @@ from .config import Config
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, resources={r"/*": {"origins":
+                                 ["http://localhost:3000",
+                                  "https://jmmabanta.github.io"]
+                                 }})
     db.init_app(app)
     app.register_blueprint(main, url_prefix='')
     return app
