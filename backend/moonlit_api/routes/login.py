29317f1ba58383f84f0638b86f6f08b4e6073d12
John M <johnmabanta51@gmail.com>
2021-11-09 16:30:27 -0700

    Reorganized API Routes

---
+++ backend/moonlit_api/routes/login.py
@@ -0,0 +1,35 @@
+import os
+from flask import Blueprint, jsonify, request
+import google.auth.transport.requests
+from google.oauth2 import id_token
+from ..database import db
+from ..models import User
+
+GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+VALID_ISSUERS = ['accounts.google.com', 'https://accounts.google.com']
+
+login = Blueprint('login', __name__)
+
+
+@login.route('/', methods=['POST'])
+# From https://github.com/adpeace/helloapp-auth-example
+def login_user():
+    token_id = request.form.get('token_id')
+    google_request = google.auth.transport.requests.Request()
+
+    user_info = id_token.verify_oauth2_token(
+        token_id, google_request, GOOGLE_CLIENT_ID)
+
+    if user_info['iss'] not in VALID_ISSUERS:
+        raise ValueError('Wrong Issuer')
+
+    google_id = hex(int(user_info['sub']))
+
+    new_user = User(google_id, user_info['name'],
+                    user_info['email'], user_info['picture'], [])
+
+    if db.session.query(User.id).filter_by(google_id=google_id).first() is None:
+        db.session.add(new_user)
+        db.session.commit()
+
+    return jsonify(user_info)
---
+++ backend/moonlit_api/routes/stocks.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import requests
+from flask import Blueprint, jsonify
+from ..models import Stock
+from ..database import db
+
+stocks = Blueprint('stocks', __name__)
+
+
+# TODO: Replace this shit API with one that can fetch multiplie prices
+def finnhubGet(location=''):
+    url = 'https://finnhub.io/api/v1' + location
+    token = {'X-Finnhub-Token': os.environ.get('FINNHUB_KEY')}
+    return requests.get(url, headers=token).json()
+
+
+@stocks.route('/', methods=['GET'])
+def main_index():
+    return "API CONNECTED! :)"
+
+
+@stocks.route('/stocks', methods=['GET'])
+def get_stocks():
+    stock_query = Stock.query.all()
+    stocks = [{'id': s.id, 'ticker': s.ticker, 'name': s.name,
+               'price': float(s.price), 'change': float(s.change)} for s in stock_query]
+    return jsonify(stocks)
+
+
+@stocks.route('/update', methods=['GET'])
+def update_stocks():
+    for s in Stock.query.order_by(Stock.price.desc()):
+        stock = Stock.query.filter_by(id=s.id).first()
+        stock.price = finnhubGet('/quote?symbol=' + stock.ticker)['c']
+        stock.change = finnhubGet('/quote?symbol=' + stock.ticker)['d']
+        print(stock.ticker, file=sys.stderr)
+        print(stock.price, file=sys.stderr)
+        print(stock.change, file=sys.stderr)
+    db.session.commit()
+    return get_stocks()
